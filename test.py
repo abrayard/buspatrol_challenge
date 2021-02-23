bc07ec740fac5916ed275ac052cda89c8826b175
Jonathan Torres <jtorres_1@outlook.com>
2021-02-23 00:31:19 -0600

    matching output is fixed for the tester

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 import requests
+import json
 
 url = "http://127.0.0.1:5000"
 
 response = requests.get(url + "/users/gene")
-print(response.json())
+user_info = json.loads(response.content)
+print(json.dumps(user_info, indent=0))
+
+#print(response.json()) # This does the job as well but does not indent the values in the JSON data. 
